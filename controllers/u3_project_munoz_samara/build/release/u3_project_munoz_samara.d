53c41dcdf0bc4322593be8df26f224d5f89c09ab
Samara <al1609086@upy.edu.mx>
2019-07-16 10:43:58 -0500

    Code re-established

--- controllers/u3_project_munoz_samara/build/release/u3_project_munoz_samara.d
+++ controllers/u3_project_munoz_samara/build/release/u3_project_munoz_samara.d
@@ -1,4 +1,4 @@
-build/release/u3_exam_munoz_samara.o: u3_exam_munoz_samara.c \
+build/release/u3_project_munoz_samara.o: u3_project_munoz_samara.c \
  /usr/local/webots/include/controller/c/webots/robot.h \
  /usr/local/webots/include/controller/c/webots/types.h \
  /usr/local/webots/include/controller/c/webots/nodes.h \
